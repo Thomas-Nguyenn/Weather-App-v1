02ca9d438f8740adf60e529063e947b87d1714ae
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-08-30 00:28:08 -0400

    adjusted api

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const request = require("request");
 
 const url =
   "https://api.darksky.net/forecast/f0dd357eefbd30c50f6189974c47806a/37.8267,-122.4233";
-
 request({ url: url, json: true }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(response.body.currently);
+  console.log(
+    response.body.daily.data[0].summary +
+      " It is currently " +
+      response.body.currently.temperature +
+      " degrees out. There is a " +
+      response.body.currently.precipProbability +
+      "% chance of rain."
+  );
 });
