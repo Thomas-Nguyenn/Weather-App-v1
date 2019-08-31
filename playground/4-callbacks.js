8adc32317c3293bb82f09ee643b9ea819b521a5c
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-08-31 02:19:39 -0400

    tested some callback functions

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,22 +1,33 @@
-setTimeout(() => {
-  console.log("Two seconds are up");
-}, 2000);
+// setTimeout(() => {
+//   console.log("Two seconds are up");
+// }, 2000);
 
-const names = ["Thomas", "Janey", "Jess"];
-const shortNames = names.filter(name => {
-  return name.length <= 4;
-});
+// const names = ["Thomas", "Janey", "Jess"];
+// const shortNames = names.filter(name => {
+//   return name.length <= 4;
+// });
 
-const geocode = (address, callback) => {
+// const geocode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = {
+//       latitude: 0,
+//       longitude: 0
+//     };
+
+//     callback(data);
+//   }, 2000);
+// };
+
+// geocode("Atlanta", data => {
+//   console.log(data);
+// });
+
+const add = (a, b, callback) => {
   setTimeout(() => {
-    const data = {
-      latitude: 0,
-      longitude: 0
-    };
-
-    return data;
+    callback(a + b);
   }, 2000);
 };
 
-const data = geocode("Atlanta");
-console.log(data);
+add(1, 4, sum => {
+  console.log(sum); // Should print: 5
+});
