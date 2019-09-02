ceacc093b870c45a42b3f145b5792fca439855b2
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-09-02 01:18:42 -0400

    working weather search

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Boston", (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
+const address = process.argv[2];
 
-  forecast(data.latitude, data.longitude, (error, data) => {
+if (!address) {
+  console.log("Please provide an address");
+} else {
+  geocode(address, (error, data) => {
     if (error) {
       return console.log(error);
     }
 
-    console.log("Error", error);
-    console.log("Data", data);
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data.location);
+      console.log(forecastData);
+    });
   });
-});
+}
+
+console.log(process.argv);
