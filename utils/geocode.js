6b6351a2ee299504f740ec937f209225f7c23a7f
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-09-05 00:27:28 -0400

    first stage

--- utils/geocode.js
+++ utils/geocode.js
@@ -6,16 +6,16 @@ const geocode = (address, callback) => {
     encodeURIComponent(address) +
     ".json?access_token=pk.eyJ1IjoiZGlnaXRhbGdyZWVkIiwiYSI6ImNqd3I0aWJpdTAyY3U0OGx5emllenUxNGkifQ.1mYYMUcKDDwzjjZeczAL4Q";
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to location services!", undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("Unable to find location. Try another search.", undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-        location: response.body.features[0].place_name
+        latitude: body.features[0].center[0],
+        longitude: body.features[0].center[1],
+        location: body.features[0].place_name
       });
     }
   });
