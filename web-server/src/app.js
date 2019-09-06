db96f87ff907a9c62acb765f49366f0712236561
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-09-06 02:46:58 -0400

    added views and hbs library

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Thomas Nguyen"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Thomas Nguyen"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    info: "Here you can find all of the information"
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
@@ -12,10 +34,6 @@ app.get("/weather", (req, res) => {
     location: "Atlanta"
   });
 });
-
-// app.com
-// app.com/help
-// app.com/about
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000.");
