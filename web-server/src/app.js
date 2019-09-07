337d9b31aaff4263514b93f378251161e72507c8
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-09-07 02:19:36 -0400

    implemented some css

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -47,11 +47,19 @@ app.get("/weather", (req, res) => {
 });
 
 app.get("/help/*", (req, res) => {
-  res.send("Help article not found");
+  res.render("404", {
+    title: "404",
+    name: "Thomas Nguyen",
+    errorMessage: "Help article not found"
+  });
 });
 
 app.get("*", (req, res) => {
-  res.send("My 404 page");
+  res.render("404", {
+    title: "404",
+    name: "Thomas Nguyen",
+    errorMessage: "Page not found."
+  });
 });
 
 app.listen(3000, () => {
