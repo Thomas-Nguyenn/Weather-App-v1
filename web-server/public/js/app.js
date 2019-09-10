3eb7a46fcce4dbbd58b8a50a50360ed480cdaf72
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-09-09 23:49:46 -0400

    Search functioning

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -12,7 +12,12 @@ fetch("http://localhost:3000/weather?address=!").then(response => {
 });
 
 const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
 
-weatherForm.addEventListener("submit", () => {
-  console.log("testing!");
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  console.log(location);
 });
